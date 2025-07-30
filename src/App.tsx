534500bf2ba7e7facf04ebb838cc6e4d479d8712
Andrew Barna <abarna@gmail.com>
2025-07-29 21:13:54 -0700

    import JSX namespace

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, JSX } from "react";
 import { Route, Routes } from "react-router";
 import { HashRouter, Link, useParams } from "react-router-dom";
 import { Breadcrumb, Card, Collapse, Button, Table } from "react-bootstrap";
